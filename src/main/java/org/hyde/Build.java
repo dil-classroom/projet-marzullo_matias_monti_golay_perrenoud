34df2c699b50c42451914f3b0d274ace2ed338d9
Pascal Perrenoud <64425383+MrDaves24@users.noreply.github.com>
2022-03-23 16:30:34 +0100

    feat: Partial implementation of buildMD

--- src/main/java/org/hyde/Build.java
+++ src/main/java/org/hyde/Build.java
@@ -3,12 +3,15 @@ package org.hyde;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.io.*;
 import java.util.concurrent.Callable;
-import java.io.File;
+
+import java.util.List;
+
+import org.commonmark.node.*;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
 @Command(name = "build")
 
 class Build implements Callable<Integer> {
@@ -29,25 +32,35 @@ class Build implements Callable<Integer> {
          if (!buildPath.isDirectory()) return 1;
       }
 
-      build(folder);
+      try {
+         build(folder);
+      } catch (IOException ignored) {
+         return 1;
+      }
 
       return 0;
    }
 
-   private static void build(File file) {
+   private static void build(File file) throws IOException {
       if (excluded.contains(file.getName())) return;
 
       if (file.isFile()) {
          buildMD(file);
       } else {
+         file.mkdir();
          for (File subfile : file.listFiles()) build(subfile);
       }
    }
 
-   private static void buildMD(File file) {
-      // Load le .md
-      // Build le .html
+   private static void buildMD(File file) throws IOException {
+      BufferedReader reader = new BufferedReader(new FileReader(file));
+      Parser parser = Parser.builder().build();
+      Node document = parser.parseReader(reader);
+      HtmlRenderer renderer = HtmlRenderer.builder().build();
+      // renderer.render(document); // return txt to write in file
+
       // Dump le contenu dans le .html correspondant :)
+      // TODO
    }
 
    public static void main(String... args) {
