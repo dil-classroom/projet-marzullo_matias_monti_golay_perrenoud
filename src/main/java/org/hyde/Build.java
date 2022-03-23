4bcb961f0ad83f4345b892ed3a7d1aa7470c5a63
Pascal Perrenoud <64425383+MrDaves24@users.noreply.github.com>
2022-03-23 16:03:26 +0100

    feat: Test given folder and build subfolder

--- src/main/java/org/hyde/Build.java
+++ src/main/java/org/hyde/Build.java
@@ -4,14 +4,27 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 import java.util.concurrent.Callable;
-
+import java.io.File;
 @Command(name = "build")
 
 class Build implements Callable<Integer> {
+   final private static String[] excluded = {"config.yaml", "build"};
+
+   @CommandLine.Parameters(index = "0", description = "The path where to build the site.")
+   private File folder;
 
    @Override
    public Integer call() {
-      System.out.println("Commande 'build'");
+      if (!(folder.isDirectory()) || !folder.exists()) return 1;
+      File build = new File(folder.getAbsolutePath() + File.separator + "build");
+
+      if (!build.exists()) {
+         if (!build.mkdir()) return 1;
+      } else {
+         if (!build.isDirectory()) return 1;
+      }
+
+      System.out.println(folder);
       return 0;
    }
 
