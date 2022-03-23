d903ee5d380ca4fca9214bcd15742c842b50b8ee
Pascal Perrenoud <64425383+MrDaves24@users.noreply.github.com>
2022-03-23 16:21:01 +0100

    feat: Recursively listing files to build

--- src/main/java/org/hyde/Build.java
+++ src/main/java/org/hyde/Build.java
@@ -3,29 +3,51 @@ package org.hyde;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.io.File;
 @Command(name = "build")
 
 class Build implements Callable<Integer> {
-   final private static String[] excluded = {"config.yaml", "build"};
+   public static final List<String> excluded = List.of("config.yaml", "build");
 
    @CommandLine.Parameters(index = "0", description = "The path where to build the site.")
    private File folder;
+   private File buildPath;
 
    @Override
    public Integer call() {
       if (!(folder.isDirectory()) || !folder.exists()) return 1;
-      File build = new File(folder.getAbsolutePath() + File.separator + "build");
+      buildPath = new File(folder.getAbsolutePath() + File.separator + "build");
 
-      if (!build.exists()) {
-         if (!build.mkdir()) return 1;
+      if (!buildPath.exists()) {
+         if (!buildPath.mkdir()) return 1;
       } else {
-         if (!build.isDirectory()) return 1;
+         if (!buildPath.isDirectory()) return 1;
       }
 
-      System.out.println(folder);
+      build(folder);
+
       return 0;
+   }
+
+   private static void build(File file) {
+      if (excluded.contains(file.getName())) return;
+
+      if (file.isFile()) {
+         buildMD(file);
+      } else {
+         for (File subfile : file.listFiles()) build(subfile);
+      }
+   }
+
+   private static void buildMD(File file) {
+      // Load le .md
+      // Build le .html
+      // Dump le contenu dans le .html correspondant :)
    }
 
    public static void main(String... args) {
