6c50f7427d6e9c08fa9e8133df4b83718db756bc
Loris199 <loris.marzullo@gmail.com>
2022-03-30 16:06:33 +0200

    feat: Ajout de la commande new

--- src/main/java/org/hyde/New.java
+++ src/main/java/org/hyde/New.java
@@ -2,16 +2,61 @@ package org.hyde;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 @Command(name = "new")
 
 class New implements Callable<Integer> {
 
+   @Parameters(paramLabel = "SITE", description = "The site to build")
+   public String site;
+
    @Override
    public Integer call() {
-      System.out.println("Commande 'new'");
+      System.out.println("new " + site);
+
+      try {
+         File directory = new File("." + site);
+         if (!directory.exists() && !directory.mkdirs()) {
+               throw new Exception("Failed to create directory");
+         }
+
+         File config = new File("." + site + "/config.yaml");
+         if (!config.exists() && !config.createNewFile()) {
+            throw new Exception("Failed to create config.yaml");
+         }
+
+         List<String> defaultConfigContent = Arrays.asList(
+                 "The first line",
+                 "The second line"
+         );
+         Path configPath = Path.of(config.getPath());
+         Files.write(configPath, defaultConfigContent, StandardCharsets.UTF_8);
+
+         File index = new File("." + site + "/index.md");
+         if (!index.exists() && index.createNewFile()) {
+            throw new Exception("Failed to create index.md");
+         }
+
+         List<String> defaultIndexContent = Arrays.asList(
+                 "The first line",
+                 "The second line"
+         );
+         Path indexPath = Path.of(index.getPath());
+         Files.write(indexPath, defaultIndexContent, StandardCharsets.UTF_8);
+
+      } catch(Exception e) {
+         e.printStackTrace();
+      }
+
       return 0;
    }
 }
