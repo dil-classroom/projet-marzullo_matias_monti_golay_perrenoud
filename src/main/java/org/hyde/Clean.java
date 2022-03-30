c80a187446703d5c83e194d5fbb207a27df70fac
joelmatias <joel.dsm@hotmail.com>
2022-03-30 15:51:25 +0200

    Ajout d'une fonction recursive afin de supprimer tout les fichiers se trouvant dans le dossier build afin de pouvoir le vider et le supprimer. Appel de cette fonction en lui donnant le paramêtre passé en ligne de commande

--- src/main/java/org/hyde/Clean.java
+++ src/main/java/org/hyde/Clean.java
@@ -10,21 +10,32 @@ import java.util.concurrent.Callable;
 
 class Clean implements Callable<Integer> {
 
+   @CommandLine.Parameters(paramLabel = "SITE", description = "The site to clean")
+   public String site;
+
    @Override
    public Integer call() {
       System.out.println("Commande 'clean'");
+
+      File path = new File(site + "/build");
+      if (path.exists()) {
+         deleteDirectory(path);
+      }
+
       return 0;
    }
 
-   public static void main(String... args) {
-      int exitCode = new CommandLine(new Clean()).execute(args);
-      String dirPath = args[1];
-
-      File path = new File(dirPath + "/build");
+   private boolean deleteDirectory(File path) {
       if (path.exists()) {
-         path.delete();
+         File[] files = path.listFiles();
+         for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory()) {
+               deleteDirectory(files[i]);
+            } else {
+               files[i].delete();
+            }
+         }
       }
-
-      System.exit(exitCode);
+      return (path.delete());
    }
 }
