ab11a4d96277068bfccd17586f559665699cf03b
Lothindir <lothindir@gmail.com>
2022-03-31 15:22:44 +0200

    feat(app): Add option to show app version
    Add the flags -v or --version to show app version. The version comes from the MANIFEST.MF file, generated by Maven. The version is the same as the pom.xml version
    
    Fixes #33

--- src/main/java/org/hyde/Hyde.java
+++ src/main/java/org/hyde/Hyde.java
@@ -1,31 +1,39 @@
 package org.hyde;
 
-import org.hyde.Build;
-import org.hyde.Clean;
-import org.hyde.New;
-import org.hyde.Serve;
-
 import java.util.concurrent.Callable;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.IVersionProvider;
+import picocli.CommandLine.Option;
 
-@Command(
-    name = "hyde",
-    description = "Jekyll's evil twin static site generator.",
-    subcommands = {New.class, Clean.class, Build.class, Serve.class})
+@Command(name = "hyde", description = "Jekyll's evil twin static site generator.", subcommands = { New.class,
+      Clean.class, Build.class, Serve.class }, versionProvider = Hyde.ManifestVersionProvider.class)
 public class Hyde implements Callable<Integer> {
 
-    public static void main(String... args) {
-    int exitCode = new CommandLine(new Hyde()).execute(args);
-    if (exitCode != 0) {
-        System.exit(exitCode);
-    }
-    }
+   @Option(names = { "-v", "--version" }, versionHelp = true, description = "Print app version")
+   boolean versionRequested;
 
-    @Override
-    public Integer call() throws Exception {
-    CommandLine.usage(this, System.out);
-    return 0;
-    }
+   public static void main(String... args) {
+      int exitCode = new CommandLine(new Hyde()).execute(args);
+      if (exitCode != 0) {
+         System.exit(exitCode);
+      }
+   }
+
+   @Override
+   public Integer call() throws Exception {
+      CommandLine.usage(this, System.out);
+      return 0;
+   }
+
+   /**
+    * {@link IVersionProvider} implementation that returns version information from
+    * the jar file's {@code /META-INF/MANIFEST.MF} file.
+    */
+   static class ManifestVersionProvider implements IVersionProvider {
+      public String[] getVersion() throws Exception {
+         return new String[] { Hyde.class.getPackage().getImplementationTitle() + " version " + Hyde.class.getPackage().getImplementationVersion() };
+      }
+   }
 }
