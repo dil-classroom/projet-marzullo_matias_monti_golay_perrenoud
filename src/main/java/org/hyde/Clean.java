a2d4c6840758eb79e7fa7233f75e867e488eccd8
Lothindir <lothindir@gmail.com>
2022-03-31 15:31:44 +0200

    refactor(pom): Replace variables with full name
    Replace pom variables with full-qualified name as per build warnings.
    For instance ${name} becomes ${project.name}

--- src/main/java/org/hyde/Clean.java
+++ src/main/java/org/hyde/Clean.java
@@ -3,6 +3,7 @@ package org.hyde;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
+import java.io.File;
 import java.util.concurrent.Callable;
 
 @Command(name = "clean")
@@ -13,5 +14,17 @@ class Clean implements Callable<Integer> {
    public Integer call() {
       System.out.println("Commande 'clean'");
       return 0;
+   }
+
+   public static void main(String... args) {
+      int exitCode = new CommandLine(new Clean()).execute(args);
+      String dirPath = args[1];
+
+      File path = new File(dirPath + "/build");
+      if (path.exists()) {
+         path.delete();
+      }
+
+      System.exit(exitCode);
    }
 }
