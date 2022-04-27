da6c9439bff1db2f772c1a330b257a766d0d2ab2
joelmatias <joel.dsm@hotmail.com>
2022-04-27 15:22:35 +0200

    Ajout de la fonction d'ouverture f'un fichier HTML

--- src/main/java/org/hyde/Serve.java
+++ src/main/java/org/hyde/Serve.java
@@ -3,15 +3,32 @@ package org.hyde;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.Callable;
 
 @Command(name = "serve")
 
 class Serve implements Callable<Integer> {
 
+   @CommandLine.Parameters(paramLabel = "SITE", description = "The site to build")
+   public String site;
+
    @Override
    public Integer call() {
       System.out.println("Commande 'serve'");
+
+      File htmlFile = new File(site + "/build/index.md.html");
+      System.out.println(htmlFile.getAbsolutePath());
+
+      try{
+         Desktop.getDesktop().browse(htmlFile.toURI());
+      }
+      catch(IOException e){
+         System.out.println("Cannot open the file");
+      }
+
       return 0;
    }
 }
