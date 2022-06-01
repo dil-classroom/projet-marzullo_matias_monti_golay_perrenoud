b6773c5b92c748b0ac862afd820fbd99edce07b7
Pascal Perrenoud <64425383+MrDaves24@users.noreply.github.com>
2022-06-01 19:13:44 +0200

    fix: Squashed fixes for #81
    * fix: Fixing "call" function
    
    Signed-off-by: Pascal Perrenoud <64425383+MrDaves24@users.noreply.github.com>
    
    * fix: Fixing "buildMD" function
    
    Signed-off-by: Pascal Perrenoud <64425383+MrDaves24@users.noreply.github.com>
    
    * fix: build function now copies other file than md to build folder
    
    Signed-off-by: Pascal Perrenoud <64425383+MrDaves24@users.noreply.github.com>
    
    * fix: on fail, function metadataTemplating should forward the exception
    
    Signed-off-by: Pascal Perrenoud <64425383+MrDaves24@users.noreply.github.com>
    
    * fix: terrible mistake on buildMD
    
    * fix: build is null-callable and creates folders for static content
    
    * fix: adding template folder to excluded list
    
    Co-authored-by: Maxim Golay <36033687+MaximGY@users.noreply.github.com>

--- src/main/java/org/hyde/Build.java
+++ src/main/java/org/hyde/Build.java
@@ -5,6 +5,7 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Objects;
@@ -18,38 +19,37 @@ import org.commonmark.node.*;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static java.nio.file.StandardWatchEventKinds.*;
 
 @Command(name = "build")
 
 class Build implements Callable<Integer> {
-   public static final List<String> excluded = List.of("config.yaml", "build");
+   public static final List<String> excluded = List.of("config.yaml", "build", "template");
 
    @CommandLine.Parameters(index = "0", description = "The path where to build the site.")
-   private Path basePath;
+   private Path basePath; // Path vers le dossier racine où build
 
    @CommandLine.Option(names = { "--watch" }, description = "If given, rebuild site when updates occur.")
    private boolean watch;
 
    @Override
    public Integer call() {
-      File baseFile = new File(String.valueOf(basePath));
-      if (!(baseFile.isDirectory()) || !baseFile.exists())
-         return 1;
-      File buildFile = new File(baseFile.getAbsolutePath() + File.separator + "build");
+      // Check that the given path is an existing directory
+      File baseFile = basePath.toFile();
+      if (!baseFile.isDirectory() || !baseFile.exists()) return 1;
 
-      // Checks or creates that the build folder exists
+      // Check that the build folder exists or try to create it
+      File buildFile = new File(baseFile.getAbsolutePath() + File.separator + "build");
       if (!buildFile.exists()) {
-         if (!buildFile.mkdir())
-            return 1;
+         if (!buildFile.mkdir()) return 1;
       } else {
-         if (!buildFile.isDirectory())
-            return 1;
+         if (!buildFile.isDirectory()) return 1;
       }
 
       // Starts the recursive building of the folder
       try {
-         build(new File(""));
+         build(null);
       } catch (IOException e) {
          e.printStackTrace();
          return 1;
@@ -87,16 +87,27 @@ class Build implements Callable<Integer> {
    }
 
    private void build(File file) throws IOException {
-      if (excluded.contains(file.getName()))
-         return;
-
-      var absPath = new File(basePath + File.separator + file);
+      File absPath;
+      if (file != null) {
+         if (excluded.contains(file.getName())) return;
+         absPath = new File(basePath + File.separator + file);
+      } else {
+         absPath = basePath.toFile();
+         file = basePath.toFile();
+      }
 
       if (absPath.isFile()) {
-         metadataTemplating(file);
-         buildMD(file);
-         if (file.toString().endsWith(".md"))
-            checkFileInclusion(file + ".html");
+         if (file.toString().endsWith(".md")) {
+            metadataTemplating(file);
+            File builtFile = buildMD(file);
+            checkFileInclusion(builtFile.toString());
+         } else {
+            File buildFile = new File(basePath + File.separator + "build" + File.separator + file);
+            if (!buildFile.getParentFile().exists() && !buildFile.getParentFile().mkdirs()) {
+               throw new RuntimeException("Impossible de créer les dossiers parents pour le contenu static.");
+            }
+            Files.copy(file.toPath(), buildFile.toPath(), REPLACE_EXISTING);
+         }
       } else {
          // Liste le contenu du dossier
          for (String subfile : Objects.requireNonNull(absPath.list())) {
@@ -113,7 +124,7 @@ class Build implements Callable<Integer> {
    private void checkFileInclusion(String file) throws IOException {
       if (!file.endsWith(".html")) return;
 
-      BufferedReader mdReader = new BufferedReader(new FileReader(basePath+File.separator+"build"+File.separator+file));
+      BufferedReader mdReader = new BufferedReader(new FileReader(file));
      
       StringBuilder data = new StringBuilder();
 
@@ -133,7 +144,7 @@ class Build implements Callable<Integer> {
 
          BufferedReader fileInclusionReader = new BufferedReader(new FileReader(basePath+File.separator+"template"+File.separator+fileName));
          System.out.println(basePath+File.separator+"template"+File.separator+fileName);
-         BufferedWriter mdWriter = new BufferedWriter(new FileWriter(basePath+File.separator+"build"+File.separator+file));
+         BufferedWriter mdWriter = new BufferedWriter(new FileWriter(file));
          StringBuilder fileInclusionData = new StringBuilder();
 
          while(fileInclusionReader.ready()){
@@ -250,6 +261,7 @@ class Build implements Callable<Integer> {
       } catch (Exception e) {
          System.err.println("An error occured while converting to HTML");
          System.err.println(e.getMessage());
+         throw e;
       }
    }
 
@@ -259,10 +271,10 @@ class Build implements Callable<Integer> {
     * @param file a .md, relative path from source folder
     * @throws IOException If the file cannot be opened
     */
-   private void buildMD(File file) throws IOException {
+   private File buildMD(File file) throws IOException {
       // Checking that the given file is a md !
       if (!file.toString().endsWith("md"))
-         return;
+         throw new IllegalArgumentException("buildMD called on another file than a MD");
 
       // Absolute path to the file, with given basePath in cmd
       File absFile = new File(basePath + File.separator + file);
@@ -272,7 +284,8 @@ class Build implements Callable<Integer> {
          throw new IllegalArgumentException("'" + absFile + "' isn't a valid file !");
 
       // Path to HTML file
-      File buildFile = new File(basePath + File.separator + "build" + File.separator + file + ".html");
+      String htmlFilename = file.toString().substring(0, file.toString().length() - 2) + "html";
+      File buildFile = new File(basePath + File.separator + "build" + File.separator + htmlFilename);
 
       // Checks that the corresponding folder exists in the build folder
       if (!buildFile.getParentFile().exists() && !buildFile.getParentFile().mkdirs()) {
@@ -291,12 +304,13 @@ class Build implements Callable<Integer> {
       // Dumps the datas to the HTML file
       try (
             FileOutputStream fos = new FileOutputStream(buildFile);
-            BufferedOutputStream bos = new BufferedOutputStream(fos)) {
-         byte[] bytes = data.getBytes();
-         bos.write(bytes);
+            BufferedOutputStream bos = new BufferedOutputStream(fos)
+      ) {
+         bos.write(data.getBytes());
       } catch (IOException e) {
          e.printStackTrace();
       }
 
+      return buildFile;
    }
 }
