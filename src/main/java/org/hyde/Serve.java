3558a0b3836efe7d5732fe0a4ea9aa59593027f1
Maxim Golay <m@xim.gy>
2022-06-08 17:36:47 +0200

    doc: Add JavaDoc comments to Serve class

--- src/main/java/org/hyde/Serve.java
+++ src/main/java/org/hyde/Serve.java
@@ -7,16 +7,25 @@ import io.javalin.http.staticfiles.Location;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
+/**
+ * Sert la dernière version construite du site via un serveur HTTP local.
+ */
 @Command(name = "serve")
-
 class Serve implements Callable<Integer> {
 
+   /**
+    * Le chemin relatif vers le site à nettoyer. Par défaut, il s'agit du dossier courant.
+    */
    @CommandLine.Parameters(arity = "0..1", paramLabel = "SITE", description = "The site to build")
    public String site = ".";
 
+   /**
+    * Méthode appelée automatiquement lors de l'invocation de "hyde serve".
+    * @return 0 une fois que le serveur web est stoppé.
+    */
    @Override
    public Integer call() {
-      System.out.println("Commande 'serve'");
+      System.out.println("Starting server...");
 
       Javalin.create(config -> {
          config.addStaticFiles(site + "/build/index.md.html", Location.EXTERNAL);
