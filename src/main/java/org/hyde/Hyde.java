4f48808247da6888d1ea13f257a718409f1454a5
Maxim Golay <m@xim.gy>
2022-06-08 17:47:23 +0200

    doc: Add JavaDoc comments to main class

--- src/main/java/org/hyde/Hyde.java
+++ src/main/java/org/hyde/Hyde.java
@@ -7,13 +7,23 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.IVersionProvider;
 import picocli.CommandLine.Option;
 
+/**
+ * Classe principale et point d'entrée de l'application.
+ */
 @Command(name = "hyde", description = "Jekyll's evil twin static site generator.", subcommands = { New.class,
       Clean.class, Build.class, Serve.class }, versionProvider = Hyde.ManifestVersionProvider.class)
 public class Hyde implements Callable<Integer> {
 
+   /**
+    * Paramètre ligne de commande imprimant la version de l'application.
+    */
    @Option(names = { "-v", "--version" }, versionHelp = true, description = "Print app version")
    boolean versionRequested;
 
+   /**
+    * Méthode d'entrée du programme.
+    * @param args Les arguments reçus en ligne de commande.
+    */
    public static void main(String... args) {
       int exitCode = new CommandLine(new Hyde()).execute(args);
       if (exitCode != 0) {
@@ -21,19 +31,26 @@ public class Hyde implements Callable<Integer> {
       }
    }
 
+   /**
+    * Méthode appelée automatiquement lors de l'invocation de "hyde".
+    * @return 0 si tout s'est bien passé.
+    */
    @Override
-   public Integer call() throws Exception {
+   public Integer call() {
       CommandLine.usage(this, System.out);
       return 0;
    }
 
    /**
-    * {@link IVersionProvider} implementation that returns version information from
-    * the jar file's {@code /META-INF/MANIFEST.MF} file.
+    * Implémentation de {@link IVersionProvider}  qui retourne les informations de versions
+    * contenues dans le fichier {@code /META-INF/MANIFEST.MF} de l'archive jar.
     */
    static class ManifestVersionProvider implements IVersionProvider {
-      public String[] getVersion() throws Exception {
-         return new String[] { Hyde.class.getPackage().getImplementationTitle() + " version " + Hyde.class.getPackage().getImplementationVersion() };
+      public String[] getVersion() {
+         return new String[] {
+              Hyde.class.getPackage().getImplementationTitle() +
+              " version " + Hyde.class.getPackage().getImplementationVersion()
+         };
       }
    }
 }
