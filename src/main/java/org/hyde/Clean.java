bbc872c437b9fc72dd6d9d585427f24d57ca6a79
Maxim Golay <m@xim.gy>
2022-06-08 17:17:30 +0200

    doc: Add JavaDoc comments to Clean class

--- src/main/java/org/hyde/Clean.java
+++ src/main/java/org/hyde/Clean.java
@@ -8,26 +8,38 @@ import java.io.IOException;
 import java.util.concurrent.Callable;
 import org.apache.commons.io.FileUtils;
 
+/**
+ * Nettoie un site des artéfacts créés par la commande "build".
+ */
 @Command(name = "clean")
-
 class Clean implements Callable<Integer> {
 
+   /**
+    * Le chemin relatif vers le site à nettoyer. Par défaut, il s'agit du dossier courant.
+    */
    @CommandLine.Parameters(arity = "0..1", paramLabel = "SITE", description = "The site to clean")
    public String site = ".";
 
+   /**
+    * Méthode appelée automatiquement lors de l'invocation de "hyde clean".
+    * @return 0 si tout s'est bien passé, -1 si le dossier n'a pas pu etre supprimé.
+    */
    @Override
    public Integer call() {
-      System.out.println("clean " + site);
+      System.out.format("Cleaning site %s... ", site);
 
       File path = new File(site + "/build");
       if (path.exists()) {
          try {
             FileUtils.deleteDirectory(path);
-         } catch (IOException e) {
-            System.err.println("Cannot delete directory " + path.getAbsolutePath());
+         }
+         catch (IOException e) {
+            System.err.format("\nError: Cannot delete directory %s", path.getAbsolutePath());
+            return -1;
          }
       }
 
+      System.out.println("Done.");
       return 0;
    }
 }
