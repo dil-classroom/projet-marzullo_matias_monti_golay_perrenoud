d5bcc571b6f7ad010e8ee79ad92553556ddd5e4a
Lothindir <lothindir@gmail.com>
2022-06-15 15:41:17 +0200

    test: add more unit and integration tests

--- src/test/java/org/hyde/CleanIT.java
+++ src/test/java/org/hyde/CleanIT.java
@@ -1,3 +1,4 @@
+/* hyde | Licenced under MIT 2022 | Golay, Marzullo, Matias, Monti & Perrenoud */
 package org.hyde;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -8,43 +9,38 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
-
 import picocli.CommandLine;
 
 public class CleanIT {
 
-   @Test
-   public void cleanShouldDeleteFolder(@TempDir File tempDirectory) {
-      assertTrue(tempDirectory.exists());
-      File buildDir = new File(tempDirectory, "build");
-      buildDir.mkdir();
-      assertTrue(buildDir.exists());
+    @Test
+    public void cleanShouldDeleteFolder(@TempDir File tempDirectory) {
+        assertTrue(tempDirectory.exists());
+        File buildDir = new File(tempDirectory, "build");
+        buildDir.mkdir();
+        assertTrue(buildDir.exists());
 
-      Hyde app = new Hyde();
-      StringWriter sw = new StringWriter();
-      CommandLine cmd = new CommandLine(app);
-      cmd.setOut(new PrintWriter(sw));
+        Hyde app = new Hyde();
+        StringWriter sw = new StringWriter();
+        CommandLine cmd = new CommandLine(app);
+        cmd.setOut(new PrintWriter(sw));
 
-      int exitCode = cmd.execute("clean", tempDirectory.getAbsolutePath());
-      assertAll(
-            () -> assertEquals(0, exitCode),
-            () -> assertFalse(buildDir.exists()));
-   }
-   
-   @Test
-   public void cleanShouldDoNothing_withNonExistingDirectory(@TempDir File tempDirectory) {
-      assertTrue(tempDirectory.exists());
+        int exitCode = cmd.execute("clean", tempDirectory.getAbsolutePath());
+        assertAll(() -> assertEquals(0, exitCode), () -> assertFalse(buildDir.exists()));
+    }
 
-      Hyde app = new Hyde();
-      StringWriter sw = new StringWriter();
-      CommandLine cmd = new CommandLine(app);
-      cmd.setOut(new PrintWriter(sw));
+    @Test
+    public void cleanShouldDoNothing_withNonExistingDirectory(@TempDir File tempDirectory) {
+        assertTrue(tempDirectory.exists());
 
-      int exitCode = cmd.execute("clean", tempDirectory.getAbsolutePath());
-      assertEquals(0, exitCode);
-   }
-   
+        Hyde app = new Hyde();
+        StringWriter sw = new StringWriter();
+        CommandLine cmd = new CommandLine(app);
+        cmd.setOut(new PrintWriter(sw));
+
+        int exitCode = cmd.execute("clean", tempDirectory.getAbsolutePath());
+        assertEquals(0, exitCode);
+    }
 }
--- src/test/java/org/hyde/NewIT.java
+++ src/test/java/org/hyde/NewIT.java
@@ -1,3 +1,4 @@
+/* hyde | Licenced under MIT 2022 | Golay, Marzullo, Matias, Monti & Perrenoud */
 package org.hyde;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -7,26 +8,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
-
 import picocli.CommandLine;
 
 public class NewIT {
-   @Test
-   public void shouldCreateNewDirectory(@TempDir File tempDirectory) {
-      Hyde app = new Hyde();
-      StringWriter sw = new StringWriter();
-      CommandLine cmd = new CommandLine(app);
-      cmd.setOut(new PrintWriter(sw));
+    @Test
+    public void shouldCreateNewDirectory(@TempDir File tempDirectory) {
+        Hyde app = new Hyde();
+        StringWriter sw = new StringWriter();
+        CommandLine cmd = new CommandLine(app);
+        cmd.setOut(new PrintWriter(sw));
 
-      int exitCode = cmd.execute("new", tempDirectory.getAbsolutePath());
+        int exitCode = cmd.execute("new", tempDirectory.getAbsolutePath());
 
-      assertAll(
-            () -> assertEquals(0, exitCode),
-            () -> assertTrue(new File(tempDirectory, "/template").exists()),
-            () -> assertTrue(new File(tempDirectory, "config.yaml").exists()),
-            () -> assertTrue(new File(tempDirectory, "index.md").exists()));
-   }
+        assertAll(
+                () -> assertEquals(0, exitCode),
+                () -> assertTrue(new File(tempDirectory, "/template").exists()),
+                () -> assertTrue(new File(tempDirectory, "config.yaml").exists()),
+                () -> assertTrue(new File(tempDirectory, "index.md").exists()));
+    }
 }
