c5c3a53c8c2705e591134569bd140ccaa3065429
Lothindir <lothindir@gmail.com>
2022-06-16 01:19:53 +0200

    refactor: lint code with spotless

--- src/main/java/org/hyde/Build.java
+++ src/main/java/org/hyde/Build.java
@@ -1,89 +1,77 @@
 /* hyde | Licenced under MIT 2022 | Golay, Marzullo, Matias, Monti & Perrenoud */
 package org.hyde;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.concurrent.Callable;
-
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.commonmark.node.*;
-import org.commonmark.parser.Parser;
-import org.commonmark.renderer.html.HtmlRenderer;
-import org.hyde.Utils.RecursiveFileWatcher;
-
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static java.nio.file.StandardWatchEventKinds.*;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.concurrent.Callable;
+import org.commonmark.node.*;
+import org.hyde.Utils.RecursiveFileWatcher;
 import org.hyde.Utils.SiteBuilder;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 @Command(name = "build")
 class Build implements Callable<Integer> {
-   public static final List<String> excluded = List.of("config.yaml", "build", "template");
+    public static final List<String> excluded = List.of("config.yaml", "build", "template");
 
-   @CommandLine.Parameters(index = "0", description = "The path where to build the site.")
-   private Path basePath; // Path vers le dossier racine où build
+    @CommandLine.Parameters(index = "0", description = "The path where to build the site.")
+    private Path basePath; // Path vers le dossier racine où build
 
-   @CommandLine.Option(names = { "--watch" }, description = "If given, rebuild site when updates occur.")
-   private boolean watch;
+    @CommandLine.Option(
+            names = {"--watch"},
+            description = "If given, rebuild site when updates occur.")
+    private boolean watch;
 
-   @Override
-   public Integer call() {
-      // Check that the given path is an existing directory
-      File baseFile = basePath.toFile();
-      if (!baseFile.isDirectory() || !baseFile.exists()) return 1;
+    @Override
+    public Integer call() {
+        // Check that the given path is an existing directory
+        File baseFile = basePath.toFile();
+        if (!baseFile.isDirectory() || !baseFile.exists()) return 1;
 
-      // Check that the build folder exists or try to create it
-      File buildFile = new File(baseFile.getAbsolutePath() + File.separator + "build");
-      if (!buildFile.exists()) {
-         if (!buildFile.mkdir()) return 1;
-      } else {
-         if (!buildFile.isDirectory()) return 1;
-      }
+        // Check that the build folder exists or try to create it
+        File buildFile = new File(baseFile.getAbsolutePath() + File.separator + "build");
+        if (!buildFile.exists()) {
+            if (!buildFile.mkdir()) return 1;
+        } else {
+            if (!buildFile.isDirectory()) return 1;
+        }
 
-      // Starts the recursive building of the folder
-      try {
-         new SiteBuilder(basePath).build(null);
-      } catch (IOException e) {
-         e.printStackTrace();
-         return 1;
-      }
+        // Starts the recursive building of the folder
+        try {
+            new SiteBuilder(basePath).build(null);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 1;
+        }
 
-      if (watch) {
-         System.out.println("Entering watch mode...");
-         try {
-            RecursiveFileWatcher watcher = new RecursiveFileWatcher(basePath);
-            RecursiveFileWatcher.FileEvent fileEvent;
-            while ((fileEvent = watcher.fetchEvent()) != null) {
-               System.out.println("Got event " + fileEvent.kind.name() + " for file " + fileEvent.path.toString());
+        if (watch) {
+            System.out.println("Entering watch mode...");
+            try {
+                RecursiveFileWatcher watcher = new RecursiveFileWatcher(basePath);
+                RecursiveFileWatcher.FileEvent fileEvent;
+                while ((fileEvent = watcher.fetchEvent()) != null) {
+                    System.out.println("Got event " + fileEvent.kind.name() + " for file " + fileEvent.path.toString());
 
-               // Update in the build directory are ignored
-               if (fileEvent.path.startsWith("build" + File.separator))
-                  continue;
+                    // Update in the build directory are ignored
+                    if (fileEvent.path.startsWith("build" + File.separator)) continue;
 
-               // Rebuild entire tree when updating config files
-               else if (fileEvent.path.endsWith("config.yaml") || fileEvent.path.endsWith("layout.html"))
-                  new SiteBuilder(basePath).build(null);
-
-               else if (ENTRY_CREATE.equals(fileEvent.kind) || ENTRY_MODIFY.equals(fileEvent.kind))
-                  new SiteBuilder(basePath).build(fileEvent.path.toFile());
-
-               else if (ENTRY_DELETE.equals(fileEvent.kind)) {
-                  // TODO: delete old files
-               }
+                    // Rebuild entire tree when updating config files
+                    else if (fileEvent.path.endsWith("config.yaml") || fileEvent.path.endsWith("layout.html"))
+                        new SiteBuilder(basePath).build(null);
+                    else if (ENTRY_CREATE.equals(fileEvent.kind) || ENTRY_MODIFY.equals(fileEvent.kind))
+                        new SiteBuilder(basePath).build(fileEvent.path.toFile());
+                    else if (ENTRY_DELETE.equals(fileEvent.kind)) {
+                        // TODO: delete old files
+                    }
+                }
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
             }
-         } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-         }
-      }
+        }
 
-      return 0;
-   }
+        return 0;
+    }
 }
--- src/main/java/org/hyde/Utils/RecursiveFileWatcher.java
+++ src/main/java/org/hyde/Utils/RecursiveFileWatcher.java
@@ -1,4 +1,7 @@
+/* hyde | Licenced under MIT 2022 | Golay, Marzullo, Matias, Monti & Perrenoud */
 package org.hyde.Utils;
+
+import static java.nio.file.StandardWatchEventKinds.*;
 
 import java.io.IOException;
 import java.nio.file.*;
@@ -7,92 +10,85 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
-import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
-import static java.nio.file.StandardWatchEventKinds.*;
-
 public class RecursiveFileWatcher {
-	private final WatchService watcher;
-	private final Map<WatchKey, Path> keys;
-	private List<WatchEvent<?>> eventsBuffer;
-	private WatchKey currentKey;
-	private boolean needsRefresh;
+    private final WatchService watcher;
+    private final Map<WatchKey, Path> keys;
+    private List<WatchEvent<?>> eventsBuffer;
+    private WatchKey currentKey;
+    private boolean needsRefresh;
 
-	public RecursiveFileWatcher(Path dir) throws IOException {
-		this.watcher = FileSystems.getDefault().newWatchService();
-		this.keys = new HashMap<WatchKey, Path>();
-		this.eventsBuffer = null;
-		this.needsRefresh = true;
-		registerAll(dir);
-	}
+    public RecursiveFileWatcher(Path dir) throws IOException {
+        this.watcher = FileSystems.getDefault().newWatchService();
+        this.keys = new HashMap<WatchKey, Path>();
+        this.eventsBuffer = null;
+        this.needsRefresh = true;
+        registerAll(dir);
+    }
 
-	/**
-	 * Register the given directory with the WatchService
-	 */
-	private void register(Path dir) throws IOException {
-		WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
-		keys.put(key, dir);
-	}
+    /**
+     * Register the given directory with the WatchService
+     */
+    private void register(Path dir) throws IOException {
+        WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+        keys.put(key, dir);
+    }
 
-	/**
-	 * Register the given directory, and all its sub-directories, with the
-	 * WatchService.
-	 */
-	private void registerAll(final Path start) throws IOException {
-		// register directory and sub-directories
-		Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
-			@Override
-			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-				register(dir);
-				return FileVisitResult.CONTINUE;
-			}
-		});
-	}
+    /**
+     * Register the given directory, and all its sub-directories, with the
+     * WatchService.
+     */
+    private void registerAll(final Path start) throws IOException {
+        // register directory and sub-directories
+        Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                register(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
 
-	private void refreshKeyAndEventBuffer() throws InterruptedException {
-		// Has to be a while, because we sometimes get empty event buffer... What a nice API !
-		while (eventsBuffer == null || eventsBuffer.isEmpty()) {
-			currentKey = watcher.take();
-			eventsBuffer = currentKey.pollEvents();
-		}
-	}
+    private void refreshKeyAndEventBuffer() throws InterruptedException {
+        // Has to be a while, because we sometimes get empty event buffer... What a nice API !
+        while (eventsBuffer == null || eventsBuffer.isEmpty()) {
+            currentKey = watcher.take();
+            eventsBuffer = currentKey.pollEvents();
+        }
+    }
 
-	public FileEvent fetchEvent() throws InterruptedException, IOException {
-		if (needsRefresh)
-			refreshKeyAndEventBuffer();
-		
-		Path dir = keys.get(currentKey);
-		WatchEvent<?> event = eventsBuffer.get(0);
-		eventsBuffer.remove(0);
-		WatchEvent.Kind kind = event.kind();
+    public FileEvent fetchEvent() throws InterruptedException, IOException {
+        if (needsRefresh) refreshKeyAndEventBuffer();
 
-		// Context for directory entry event is the file name of entry
-		WatchEvent<Path> ev = (WatchEvent<Path>) event;
-		Path name = ev.context();
-		Path child = dir.resolve(name);
+        Path dir = keys.get(currentKey);
+        WatchEvent<?> event = eventsBuffer.get(0);
+        eventsBuffer.remove(0);
+        WatchEvent.Kind kind = event.kind();
 
-		// Register new directories
-		if (kind == ENTRY_CREATE)
-			registerAll(child);
+        // Context for directory entry event is the file name of entry
+        WatchEvent<Path> ev = (WatchEvent<Path>) event;
+        Path name = ev.context();
+        Path child = dir.resolve(name);
 
-		// Cleanup for next call
-		if (eventsBuffer.isEmpty()) {
-			if (!currentKey.reset())
-				keys.remove(currentKey);
+        // Register new directories
+        if (kind == ENTRY_CREATE) registerAll(child);
 
-			needsRefresh = true;
-		}
+        // Cleanup for next call
+        if (eventsBuffer.isEmpty()) {
+            if (!currentKey.reset()) keys.remove(currentKey);
 
-		return new FileEvent(child, kind);
-	}
+            needsRefresh = true;
+        }
 
-	public static class FileEvent {
-		public final Path path;
-		public final WatchEvent.Kind<?> kind;
+        return new FileEvent(child, kind);
+    }
 
-		FileEvent(Path path, WatchEvent.Kind<?> kind) {
-			this.path = path;
-			this.kind = kind;
-		}
-	}
+    public static class FileEvent {
+        public final Path path;
+        public final WatchEvent.Kind<?> kind;
+
+        FileEvent(Path path, WatchEvent.Kind<?> kind) {
+            this.path = path;
+            this.kind = kind;
+        }
+    }
 }
